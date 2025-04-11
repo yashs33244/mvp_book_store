e083cdbb4319370e74a9b8d07c9662e84291ed59
Yash Singh <myemail>
2025-04-12 01:39:51 +0530

    v3 ready

--- be/src/index.ts
+++ be/src/index.ts
@@ -18,6 +18,12 @@ const port = process.env.PORT || 3001;
 // Security middleware
 
 
+app.use(cors({
+  origin: ['http://books.yashprojects.online', 'https://books.yashprojects.online'], 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // Logging middleware
 app.use(morgan('dev'));
 
@@ -27,7 +33,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
 app.use(express.json());
 
 // Routes
